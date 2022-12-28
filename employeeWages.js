58072f7c0f610664956990fcad9b2e6629f1e0ed
SOURAV <devbhsourav@gmail.com>
2022-12-28 08:52:08 +0530

    [sourav]add : find hours function

--- employeeWages.js
+++ employeeWages.js
@@ -4,10 +4,27 @@ const IS_FULL_TIME = 1;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-
-//UC2
 let empWage = 0;
 let empCheck = Math.floor(Math.random()* 10 % 2);
+
+//UC3
+function getWorkingHours(empCheck){
+    switch(empCheck){
+        case IS_PART_TINE:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
+}
+
+let empHours = getWorkingHours(empCheck);
+empWage = WAGE_PER_HOUR * empHours;
+console.log("Function to find employee salary :" + empWage)
+
+
+//UC2
 switch(empCheck){
     case IS_PART_TINE:
         empWage = WAGE_PER_HOUR * PART_TIME_HOURS;
