b0c5c54a729e158fcab9602d8afcec12794b7595
SOURAV <devbhsourav@gmail.com>
2022-12-28 08:28:20 +0530

    UC1-employeeAttandence

---
+++ employeeWages.js
@@ -0,0 +1,8 @@
+const IS_PRESENT = 0;
+let attandence = Math.floor(Math.random()* 10 % 2);
+if(IS_PRESENT == attandence){
+    console.log("Employee is present");
+}
+else{
+    console.log("Employee is absent");
+}
